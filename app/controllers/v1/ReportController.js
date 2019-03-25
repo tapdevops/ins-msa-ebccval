8b59f848f26913357ab5962fdb044cd7f3ea4115
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2019-03-25 16:13:31 +0700

    Ferdinand

--- app/controllers/v1/ReportController.js
+++ app/controllers/v1/ReportController.js
@@ -1,5 +1,17 @@
 /*
  |--------------------------------------------------------------------------
+ | App Setup
+ |--------------------------------------------------------------------------
+ |
+ | Untuk menghandle models, libraries, helper, node modules, dan lain-lain
+ |
+ */
+ 	// Models
+	const EBCCValidationHeaderModel = require( _directory_base + '/app/models/EBCCValidationHeaderModel.js' );
+	const EBCCValidationDetailModel = require( _directory_base + '/app/models/EBCCValidationDetailModel.js' );
+
+/*
+ |--------------------------------------------------------------------------
  | Versi 1.0.0
  |--------------------------------------------------------------------------
  */
@@ -9,9 +21,20 @@
 	  * @return json
 	  * --------------------------------------------------------------------
 	*/
-	exports.web_report_per_baris_v_1_0 = ( req, res ) => {
+	exports.web_report_per_baris_v_1_0 = async ( req, res ) => {
+		var query = await EBCCValidationHeaderModel.aggregate([
+			{
+				"$lookup": {
+					"from": "TR_D_EBCC_VALIDATION",
+					"localField": "EBCC_VALIDATION_CODE",
+					"foreignField": "EBCC_VALIDATION_CODE",
+					"as": "DETAIL"
+		        }
+		    }
+		]);
 		res.json( {
 			message: "OK",
-			auth: req.auth
+			auth: req.auth,
+			query: query
 		} );
 	}
