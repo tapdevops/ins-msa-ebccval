e31c3b2841ce48377438ecd0f334dff65510c744
Ferd Shinoda <mac@Macs-MacBook-Pro-2.local>
2019-05-28 14:59:58 +0700

    Ferdinand

--- config/database.js
+++ config/database.js
@@ -9,12 +9,17 @@
  |
  */
 	module.exports = {
-		production: {
-			url: 'mongodb://s_ebcc_validation:38ccvalid2019@dbapp.tap-agri.com:4848/s_ebcc_validation?authSource=s_ebcc_validation',
-			ssl: false
-		},
+		
 		development: {
 			url: 'mongodb://s_ebcc_validation:s_ebcc_validation@dbappdev.tap-agri.com:4848/s_ebcc_validation?authSource=s_ebcc_validation',
+			ssl: false
+		},
+		qa: {
+			url: 'mongodb://s_ebcc_validation:s_ebcc_validation@dbappdev.tap-agri.com:4848/s_ebcc_validation?authSource=s_ebcc_validation',
+			ssl: false
+		},
+		production: {
+			url: 'mongodb://s_ebcc_validation:38ccvalid2019@dbapp.tap-agri.com:4848/s_ebcc_validation?authSource=s_ebcc_validation',
 			ssl: false
 		}
 	}
