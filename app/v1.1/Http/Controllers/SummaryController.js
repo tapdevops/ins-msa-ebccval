746ca0f15d1ff21bc4b3b2b1c364c803680c822c
Ihsan Husaeri <ihusaeri@gmail.com>
2019-08-26 13:22:39 +0700

    Ihsan

--- app/v1.1/Http/Controllers/SummaryController.js
+++ app/v1.1/Http/Controllers/SummaryController.js
@@ -9,7 +9,7 @@
  	// Models
  	const EBCCValidationHeaderModel = require( _directory_base + '/app/v1.1/Http/Models/EBCCValidationHeaderModel.js' );
  	const KualitasModel = require( _directory_base + '/app/v1.1/Http/Models/KualitasModel.js' );
-
+	const SummaryWeeklyModel = require( _directory_base + '/app/v1.1/Http/Models/SummaryWeeklyModel.js' );
  	// Node Modules
  	const MomentTimezone = require( 'moment-timezone' );
 
@@ -68,6 +68,28 @@
 			date_min_1_week.setDate( date_min_1_week.getDate() - 7 );
 			date_min_1_week = parseInt( MomentTimezone( date_min_1_week ).tz( "Asia/Jakarta" ).format( "YYYYMMDD" ) + '000000' );
 
+		var ebcc_query = await EBCCValidationHeaderModel.aggregate( [
+			{
+				"$match": {
+					"INSERT_TIME": {
+						"$lte": max_date
+					},
+					"INSERT_USER": req.auth.USER_AUTH_CODE
+				}
+			},
+			{
+				"$count": "jumlah"
+			}
+		] );
+   
+		var set = new SummaryWeeklyModel( {
+			"TOTAL_EBCC": ebcc_query[0].jumlah,
+			"SUMMARY_DATE": parseInt( date_now.toString().substr( 0, 8 ) ),
+			"IS_VIEW": 0,
+			"INSERT_USER": authCode,
+			"INSERT_TIME": Helper.date_format( 'now', 'YYYYMMDDhhmmss' )
+		} );
+		console.log( set )
 		return res.json( {
 			status: true,
 			message: "Success!",
